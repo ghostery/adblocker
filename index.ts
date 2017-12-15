26fc215ce18960c4d456eb0fd541e14368f16038
Rémi Berson <remi@cliqz.com>
2017-12-15 11:37:24 +0100

    Improve build commands

--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-export { f, parseList } from './src/parsing/list';
 export { default as CosmeticInjection } from './src/cosmetics-injection';
 export { default as Engine } from './src/filters-engine';
-
-export default function adblocker() {
-  console.log('I am adblocker!');
-}
+export { deserializeEngine } from './src/serialization';
