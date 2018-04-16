d6d1d4a0a698509414ac80dae159fe991ab6904a
Tim Palade <timotei@cliqz.com>
2018-04-16 13:42:42 +0200

    test rules as they are converted + bug fix for last commit (allowedLoadTypes was not used)

--- test/safari-rules.test.js
+++ test/safari-rules.test.js
@@ -1,55 +1,29 @@
 /* globals jest, expect, test */
 
-// import {} from 'jest';
-// import { f } from '../src/parsing/list';
-// import { convertCosmetics, convertFilter } from '../src/convertion/safari-rules';
+import { loadAllLists } from '../assets/load';
+import { convertCosmetics, convertFilter } from '../src/convertion/safari-rules';
+import { parseList } from '../src/parsing/list';
 
-// expect.extend({
-//   toBeConvertedTo(filter, expectedSafariRule) {
-//     let safariRule;
-//     if (filter.isNetworkFilter()) {
-//       safariRule = convertFilter(filter);
-//     } else {
-//       safariRule = convertCosmetics(filter);
-//     }
-
-//     try {
-//       expect(safariRule).toEqual(expectedSafariRule);
-//     } catch (ex) {
-//       return {
-//         message: () =>
-//           `expected ${filter.toString()} to be converted to ${JSON.stringify(expectedSafariRule)}, got: ${JSON.stringify(safariRule)}`,
-//         pass: false,
-//       };
-//     }
-
-//     return {
-//       message: () =>
-//         `expected ${filter.toString()} to not be converted to ${JSON.stringify(expectedSafariRule)}`,
-//       pass: true,
-//     };
-//   },
-// });
-
-// describe('#convertFilter', () => {
-//   it('converts', () => {
-//     expect(f`||foo.com`).toBeConvertedTo({
-//       action: { type: 'block' },
-//       trigger: {
-//         'url-filter': '(.*)?foo.com/',
-//         'url-filter-is-case-sensitive': false,
-//         'unless-domain': [],
-//         'load-type': []
-//       }
-//     });
-//   });
-// });
-
-// describe('#convertCosmetics', () => {
-// });
-
-jest.dontMock('../../../converted_rules.json');
-const json = require('../../../converted_rules.json');
+expect.extend({
+  toContainKeys(allowedKeys, keys) {
+    for (let i = 0; i < keys.length; i += 1) {
+      try {
+        expect(allowedKeys.has(keys[i])).toBe(true);
+      } catch (ex) {
+        return {
+          message: () =>
+            `expected ${allowedKeys} to contain ${keys[i]}`,
+          pass: false,
+        };
+      }
+    }
+    return {
+      message: () =>
+        'pass',
+      pass: true,
+    };
+  },
+});
 
 const allowedTriggerKeys = new Set([
   'url-filter', 'url-filter-is-case-sensitive', 'if-domain', 'unless-domain', 'resource-type', 'load-type', 'if-top-url', 'unless-top-url',
@@ -63,164 +37,188 @@ const allowedResourceTypes = new Set([
   'document', 'image', 'style-sheet', 'script', 'font', 'raw', 'svg-document', 'media', 'popup',
 ]);
 
-const allowedLoadTypeValues = new Set(['first-party', 'third-party']);
+const allowedLoadTypes = new Set(['first-party', 'third-party']);
 
 const allowedActions = new Set(['block', 'block-cookies', 'css-display-none', 'ignore-previous-rules', 'make-https']);
 
-function checkObjectKeysAllowed(keys, allowedKeys) {
-  for (let i = 0; i < keys.length; i += 1) {
-    expect(allowedKeys.has(keys[i])).toBe(true);
+function testRule(rule) {
+  // console.log(rule);
+  // const punycode = require('punycode');
+
+  // TODO: test if string is punycode encoded.
+
+  // rule must be an object
+  expect(typeof rule).toBe('object');
+
+  // rule must have only 2 keys, action and trigger, of type object
+  expect(Object.keys(rule).length).toBe(2);
+  expect(typeof rule.action).toBe('object');
+  expect(typeof rule.trigger).toBe('object');
+
+  // trigger must have a non-empty url-filter, of type string
+  const urlfilter = rule.trigger['url-filter'];
+  expect(typeof urlfilter).toBe('string');
+  expect(urlfilter).not.toBe('');
+
+  // trigger can only have the following keys
+  expect(allowedTriggerKeys).toContainKeys(Object.keys(rule.trigger));
+
+  // action must have a type, with a value of type string
+  expect(typeof rule.action.type).toBe('string');
+  expect(rule.action.type).not.toBe('');
+
+  // action can only have the following keys
+  expect(allowedActionKeys).toContainKeys(Object.keys(rule.action));
+
+  // url-filter must contain a valid regex
+  // test if the value is a valid regex
+  let value = rule.trigger['url-filter'];
+  let isValid = true;
+  try {
+    new RegExp(value);
+  } catch (e) {
+    isValid = false;
+  }
+  expect(isValid).toBe(true);
+
+  // url-filter-is-case-sensitive should be a boolean
+  value = rule.trigger['url-filter-is-case-sensitive'];
+  if (value !== undefined) {
+    expect(typeof value).toBe('boolean');
+  }
+
+  // if-domain must be a non-empty array of punnycode lowercase strings
+  value = rule.trigger['if-domain'];
+  if (value !== undefined) {
+    expect(Array.isArray(value)).toBe(true);
+    expect(value.length).not.toBeLessThan(1);
+    for (let j = 0; j < value.length; j += 1) {
+      const elem = value[j];
+      expect(typeof elem).toBe('string');
+      expect(elem === elem.toLowerCase()).toBe(true);
+      // TODO: test if string is punycode encoded
+    }
+  }
+
+  // unless-domain must be a non-empty array of punnycode lowercase strings
+  value = rule.trigger['unless-domain'];
+  if (value !== undefined) {
+    expect(Array.isArray(value)).toBe(true);
+    expect(value.length).not.toBeLessThan(1);
+    for (let j = 0; j < value.length; j += 1) {
+      const elem = value[j];
+      expect(typeof elem).toBe('string');
+      expect(elem === elem.toLowerCase()).toBe(true);
+      // TODO: test if string is punycode encoded
+    }
+  }
+
+  // resource-type must be a non-empty array of strings with the following values
+  value = rule.trigger['resource-type'];
+  if (value !== undefined) {
+    expect(value.length).not.toBeLessThan(1);
+    expect(allowedResourceTypes).toContainKeys(value);
+  }
+
+  // load-type must be a non-empty array of strings with the following values
+  value = rule.trigger['load-type'];
+  if (value !== undefined) {
+    expect(value.length).not.toBeLessThan(1);
+    expect(allowedLoadTypes).toContainKeys(value);
+  }
+
+  // if-top-url must be a non-empty array of punnycode lowercase strings
+  value = rule.trigger['if-top-url'];
+  if (value !== undefined) {
+    expect(Array.isArray(value)).toBe(true);
+    expect(value.length).not.toBeLessThan(1);
+    for (let j = 0; j < value.length; j += 1) {
+      const elem = value[j];
+      expect(typeof elem).toBe('string');
+      expect(elem === elem.toLowerCase()).toBe(true);
+      // TODO: test if string is punycode encoded
+    }
+  }
+
+  // unless-top-url must be a non-empty array of punnycode lowercase strings
+  value = rule.trigger['unless-top-url'];
+  if (value !== undefined) {
+    expect(Array.isArray(value)).toBe(true);
+    expect(value.length).not.toBeLessThan(1);
+    for (let j = 0; j < value.length; j += 1) {
+      const elem = value[j];
+      expect(typeof elem).toBe('string');
+      expect(elem === elem.toLowerCase()).toBe(true);
+      // TODO: test if string is punycode encoded
+    }
+  }
+
+  // type must be a string with the following possible values
+  value = rule.action.type;
+  expect(allowedActions.has(value)).toBe(true);
+
+  // if type is css-display-none, a non-empty selector must exist
+  if (rule.action.type === 'css-display-none') {
+    expect(rule.action.selector).not.toBeUndefined();
+    expect(rule.action.selector).not.toBe('');
+  }
+
+  // selector must be a non-empty string
+  value = rule.action.selector;
+  if (value !== undefined) {
+    expect(typeof value).toBe('string');
+    expect(value).not.toBe('');
+  }
+
+  // trigger cannot contain both if-domain and unless-domain
+  const ifdomain = rule.trigger['if-domain'];
+  const unlessdomain = rule.trigger['unless-domain'];
+  if (ifdomain !== undefined) {
+    expect(unlessdomain).toBeUndefined();
+  } else if (unlessdomain !== undefined) {
+    expect(ifdomain).toBeUndefined();
+  }
+
+  // trigger cannot contain both if-top-url and unless-top-url
+  const ifurl = rule.trigger['if-top-url'];
+  const unlessurl = rule.trigger['unless-top-url'];
+  if (ifurl !== undefined) {
+    expect(unlessurl).toBeUndefined();
+  } else if (unlessurl !== undefined) {
+    expect(ifurl).toBeUndefined();
   }
 }
 
 test('rule must be a valid safari rule', () => {
-  for (let i = 0; i < json.length; i += 1) {
-    // describe('convert Filters', () => {
-    const rule = json[i];
-    //const punycode = require('punycode');
 
-    // TODO: test if string is punycode encoded.
-    // TODO: look at the Apple tests. Make sure we implement all of them.
+  const { networkFilters, cosmeticFilters } = parseList(loadAllLists());
+  let nullCount = 0;
 
-    // rule must be an object
-    expect(typeof rule).toBe('object');
-
-    // rule must have only 2 keys, action and trigger, of type object
-    expect(Object.keys(rule).length).toBe(2);
-    expect(typeof rule.action).toBe('object');
-    expect(typeof rule.trigger).toBe('object');
-
-    // trigger must have a non-empty url-filter, of type string
-    const urlfilter = rule.trigger['url-filter'];
-    expect(typeof urlfilter).toBe('string');
-    expect(urlfilter).not.toBe('');
-
-    // trigger can only have the following keys
-    checkObjectKeysAllowed(Object.keys(rule.trigger), allowedTriggerKeys);
-
-    // action must have a type, with a value of type string
-    expect(typeof rule.action.type).toBe('string');
-    expect(rule.action.type).not.toBe('');
-
-    // action can only have the following keys
-    checkObjectKeysAllowed(Object.keys(rule.action), allowedActionKeys);
-
-    // url-filter must contain a valid regex
-    // test if the value is a valid regex
-    let value = rule.trigger['url-filter'];
-    let isValid = true;
-    try {
-      new RegExp(value);
-    } catch (e) {
-      isValid = false;
-    }
-    expect(isValid).toBe(true);
-
-    // url-filter-is-case-sensitive should be a boolean
-    value = rule.trigger['url-filter-is-case-sensitive'];
-    if (value !== undefined) {
-      expect(typeof value).toBe('boolean');
-    }
-
-    // if-domain must be a non-empty array of punnycode lowercase strings
-    value = rule.trigger['if-domain'];
-    if (value !== undefined) {
-      expect(Array.isArray(value)).toBe(true);
-      expect(value.length).not.toBeLessThan(1);
-      for (let j = 0; j < value.length; j += 1) {
-        const elem = value[j];
-        expect(typeof elem).toBe('string');
-        expect(elem === elem.toLowerCase()).toBe(true);
-        // TODO: test if string is punycode encoded
+  for (let i = 0; i < cosmeticFilters.length; i += 1) {
+    const cosmetic = cosmeticFilters[i];
+    const rule = convertCosmetics(cosmetic);
+    if (rule !== null && rule !== undefined) {
+      try {
+        testRule(rule);
+      } catch (ex) {
+        break;
       }
+    } else {
+      nullCount += 1;
     }
+  }
 
-    // unless-domain must be a non-empty array of punnycode lowercase strings
-    value = rule.trigger['unless-domain'];
-    if (value !== undefined) {
-      expect(Array.isArray(value)).toBe(true);
-      expect(value.length).not.toBeLessThan(1);
-      for (let j = 0; j < value.length; j += 1) {
-        const elem = value[j];
-        expect(typeof elem).toBe('string');
-        expect(elem === elem.toLowerCase()).toBe(true);
-        // TODO: test if string is punycode encoded
+  for (let j = 0; j < networkFilters.length; j += 1) {
+    const filter = networkFilters[j];
+    const rule = convertFilter(filter);
+    if (rule !== null && rule !== undefined) {
+      try {
+        testRule(rule);
+      } catch (ex) {
+        break;
       }
-    }
-
-    // resource-type must be a non-empty array of strings with the following values
-    expect(rule.trigger['resource-type'].length).not.toBeLessThan(1);
-    checkObjectKeysAllowed(
-      rule.trigger['resource-type'] || [],
-      allowedResourceTypes,
-    );
-
-    // load-type must be a non-empty array of strings with the following values
-    expect(rule.trigger['load-type'].length).not.toBeLessThan(1);
-    checkObjectKeysAllowed(
-      rule.trigger['load-type'] || [],
-      allowedLoadTypeValues,
-    );
-
-    // if-top-url must be a non-empty array of punnycode lowercase strings
-    value = rule.trigger['if-top-url'];
-    if (value !== undefined) {
-      expect(Array.isArray(value)).toBe(true);
-      expect(value.length).not.toBeLessThan(1);
-      for (let j = 0; j < value.length; j += 1) {
-        const elem = value[j];
-        expect(typeof elem).toBe('string');
-        expect(elem === elem.toLowerCase()).toBe(true);
-        // TODO: test if string is punycode encoded
-      }
-    }
-
-    // unless-top-url must be a non-empty array of punnycode lowercase strings
-    value = rule.trigger['unless-top-url'];
-    if (value !== undefined) {
-      expect(Array.isArray(value)).toBe(true);
-      expect(value.length).not.toBeLessThan(1);
-      for (let j = 0; j < value.length; j += 1) {
-        const elem = value[j];
-        expect(typeof elem).toBe('string');
-        expect(elem === elem.toLowerCase()).toBe(true);
-        // TODO: test if string is punycode encoded
-      }
-    }
-
-    // type must be a string with the following possible values
-    value = rule.action.type;
-    expect(allowedActions.has(value)).toBe(true);
-
-    // if type is css-display-none, a non-empty selector must exist
-    if (rule.action.type === 'css-display-none') {
-      expect(rule.action.selector).not.toBeUndefined();
-      expect(rule.action.selector).not.toBe('');
-    }
-
-    // selector must be a non-empty string
-    value = rule.action.selector;
-    if (value !== undefined) {
-      expect(typeof value).toBe('string');
-      expect(value).not.toBe('');
-    }
-
-    // trigger cannot contain both if-domain and unless-domain
-    const ifdomain = rule.trigger['if-domain'];
-    const unlessdomain = rule.trigger['unless-domain'];
-    if (ifdomain !== undefined) {
-      expect(unlessdomain).toBeUndefined();
-    } else if (unlessdomain !== undefined) {
-      expect(ifdomain).toBeUndefined();
-    }
-
-    // trigger cannot contain both if-top-url and unless-top-url
-    const ifurl = rule.trigger['if-top-url'];
-    const unlessurl = rule.trigger['unless-top-url'];
-    if (ifurl !== undefined) {
-      expect(unlessurl).toBeUndefined();
-    } else if (unlessurl !== undefined) {
-      expect(ifurl).toBeUndefined();
+    } else {
+      nullCount += 1;
     }
   }
 });
