ae1abba6d4fb0df4cc1f06750a24cfe767544f13
Rémi Berson <remi@cliqz.com>
2018-04-24 21:31:53 +0200

    Fix convertion of domain options

--- src/convertion/safari-rules.ts
+++ src/convertion/safari-rules.ts
@@ -111,9 +111,9 @@ export function convertFilter(filter: NetworkFilter) {
   if (filter.hasOptDomains() && filter.hasOptNotDomains()) {
     return null;
   } else if (filter.hasOptDomains()) {
-    trigger['if-domain'] = [...filter.getOptDomains()].filter(isAscii).map(d => '*' + d.toLowerCase());
+    trigger['if-domain'] = [...filter.getOptDomains()].filter(isAscii).map(d => '.*' + d.toLowerCase());
   } else if (filter.hasOptNotDomains()) {
-    trigger['unless-domain'] = [...filter.getOptNotDomains()].filter(isAscii).map(d => '*' + d.toLowerCase());
+    trigger['unless-domain'] = [...filter.getOptNotDomains()].filter(isAscii).map(d => '.*' + d.toLowerCase());
   }
 
   // resource-type
