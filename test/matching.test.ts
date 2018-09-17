650be61ea14af2a5ce37a2c1ee02fde0e397114c
Rémi Berson <remi@cliqz.com>
2018-09-17 10:30:41 +0200

    Add matching tests against requests database

--- test/matching.test.ts
+++ test/matching.test.ts
@@ -2,7 +2,10 @@ import matchCosmeticFilter from '../src/matching/cosmetics';
 import matchNetworkFilter, { isAnchoredByHostname } from '../src/matching/network';
 
 import { f } from '../src/parsing/list';
+import { parseNetworkFilter } from '../src/parsing/network-filter';
 import { processRawRequest } from '../src/request/raw';
+
+import requests from './data/requests';
 
 // TODO add tests with positive match in parameters, fragment, etc. (all
 // possible parts of a URL).
@@ -52,6 +55,53 @@ expect.extend({
       pass: false,
     };
   },
+});
+
+interface Dict {
+  [s: string]: number;
+}
+
+const types: Dict = {
+  // maps string (web-ext) to int (FF cpt)
+  beacon: 19,
+  csp_report: 17,
+  font: 14,
+  image: 3,
+  imageset: 21,
+  main_frame: 6,
+  media: 15,
+  object: 5,
+  object_subrequest: 12,
+  other: 1,
+  ping: 10,
+  script: 2,
+  stylesheet: 4,
+  sub_frame: 7,
+  web_manifest: 22,
+  websocket: 16,
+  xbl: 9,
+  xml_dtd: 13,
+  xmlhttprequest: 11,
+  xslt: 18,
+};
+
+it('against list of requests', () => {
+  requests.forEach(({ filter, exception, cpt, sourceUrl, url }) => {
+    let networkFilter;
+    if (filter !== undefined) {
+      networkFilter = parseNetworkFilter(filter);
+    } else if (exception !== undefined) {
+      networkFilter = parseNetworkFilter(exception);
+    }
+
+    expect(networkFilter).not.toBeUndefined();
+    expect(networkFilter).not.toBeNull();
+    expect(networkFilter).toMatchRequest({
+      cpt: types[cpt],
+      sourceUrl,
+      url,
+    });
+  });
 });
 
 describe('#isAnchoredByHostname', () => {
