05364c7f9b5ff72d852a9349f7d0f124011387bf
Rémi <remusao@users.noreply.github.com>
2019-01-30 11:57:03 +0100

    Bump engine version to avoid issues while deserializing (#95)

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -10,7 +10,7 @@ import NetworkFilterBucket from './bucket/network';
 
 import { createStylesheet } from '../content/injection';
 
-export const ENGINE_VERSION = 17;
+export const ENGINE_VERSION = 18;
 
 // Polyfill for `btoa`
 function btoaPolyfill(buffer: string): string {
