cae00339bc6459c7e31fcf931b3208e8faca16b3
Rémi <remusao@users.noreply.github.com>
2019-04-17 10:42:10 +0200

    Update local assets + update tests (#129)

--- assets/update_lists.sh
+++ assets/update_lists.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-curl 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt' > ./easylist-downloads.adblockplus.org/antiadblockfilters.txt &
-curl 'https://easylist-downloads.adblockplus.org/easylistgermany.txt' > ./easylist.to/easylistgermany/easylistgermany.txt &
-curl 'https://easylist.to/easylist/easylist.txt' > ./easylist.to/easylist/easylist.txt &
+# uBlock Origin
 curl 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt' > ./raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt &
 curl 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt' > ./raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt &
 curl 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt' > ./raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt &
 curl 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resources.txt' > ./raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resources.txt &
 curl 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt' > ./raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt &
 curl 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt' > ./raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt &
+
+# Peter Lowe
 curl 'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=0&mimetype=plaintext' > ./pgl.yoyo.org/adservers/serverlist.txt
+
+# Easylist
 curl 'https://easylist.to/easylist/easyprivacy.txt' > ./easylist.to/easylist/easyprivacy.txt &
+curl 'https://easylist.to/easylist/easylist.txt' > ./easylist.to/easylist/easylist.txt &
+curl 'https://easylist-downloads.adblockplus.org/easylistgermany.txt' > ./easylist.to/easylistgermany/easylistgermany.txt &
+curl 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt' > ./easylist-downloads.adblockplus.org/antiadblockfilters.txt &
+
+# AdGuard
+curl 'https://filters.adtidy.org/extension/ublock/filters/11.txt' > ./adguard/mobile.txt &
 
 # Wait for child processes to terminate
 wait
--- test/utils.ts
+++ test/utils.ts
@@ -7,12 +7,15 @@ function readAsset(filepath: string) {
 
 export function loadAllLists() {
   return [
+    'assets/adguard/mobile.txt',
     'assets/easylist-downloads.adblockplus.org/antiadblockfilters.txt',
     'assets/easylist.to/easylist/easylist.txt',
     'assets/easylist.to/easylist/easyprivacy.txt',
     'assets/easylist.to/easylistgermany/easylistgermany.txt',
+    'assets/pgl.yoyo.org/adservers/serverlist.txt',
     'assets/raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt',
     'assets/raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt',
+    'assets/raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt',
     'assets/raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt',
     'assets/raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt',
   ]
