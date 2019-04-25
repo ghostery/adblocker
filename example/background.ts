7a26af7cab7b8c4bf690b216b678cc0f8d1433e3
Rémi Berson <remi@cliqz.com>
2019-04-25 15:38:08 +0200

    Remove debug log

--- example/background.ts
+++ example/background.ts
@@ -113,7 +113,6 @@ loadAdblocker().then((engine) => {
   // some of them (or redirect).
   chrome.webRequest.onBeforeRequest.addListener(
     (details) => {
-      console.error(details.url);
       const result = engine.match(requestFromDetails(details));
 
       if (result.redirect) {
