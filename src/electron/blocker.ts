30770164b8a039e651e04416ea190c4c4a489cc4
Sential <sentialx@gmail.com>
2019-06-27 19:53:00 +0200

    Add ipc message handler in main process

--- src/electron/blocker.ts
+++ src/electron/blocker.ts
@@ -7,10 +7,12 @@
  */
 
 // Type definition
-import * as Electron from 'electron';
+import { ipcMain } from 'electron';
 
 import Engine from '../engine/engine';
 import Request from '../request';
+import { IBackgroundCallback, IMessageFromBackground } from '../../cosmetics';
+import { parse } from 'tldts';
 
 /**
  * Wrap `FiltersEngine` into a Electron-friendly helper class.
@@ -19,6 +21,92 @@ export default class ElectronBlocker extends Engine {
   public enableBlockingInSession(ses: Electron.Session) {
     ses.webRequest.onBeforeRequest({ urls: ['<all_urls>'] }, this.onRequest);
     ses.setPreloads(['./content.js']);
+
+    ipcMain.on('get-cosmetic-filters', this.onGetCosmeticFilters);
+  }
+
+  private onGetCosmeticFilters = (
+    e: Electron.IpcMessageEvent,
+    id: string,
+    msg: IBackgroundCallback & { action?: string }
+  ): void => {
+    // Extract hostname from sender's URL
+    const url = e.sender.getURL();
+    const frameId = 0;
+    const parsed = parse(url);
+    const hostname = parsed.hostname || '';
+    const domain = parsed.domain || '';
+
+    // Once per tab/page load we inject base stylesheets. These are always
+    // the same for all frames of a given page because they do not depend on
+    // a particular domain and cannot be cancelled using unhide rules.
+    // Because of this, we specify `allFrames: true` when injecting them so
+    // that we do not need to perform this operation for sub-frames.
+    if (frameId === 0 && msg.lifecycle === 'start') {
+      const { active, styles } = this.getCosmeticsFilters({
+        domain,
+        hostname,
+        url,
+
+        classes: msg.classes,
+        hrefs: msg.hrefs,
+        ids: msg.ids,
+
+        // This needs to be done only once per tab
+        getBaseRules: true,
+        getInjectionRules: false,
+        getRulesFromDOM: false,
+        getRulesFromHostname: false,
+      });
+
+      if (active === false) {
+        return;
+      }
+
+      this.injectStyles(e.sender, styles);
+    }
+
+    // Separately, requests cosmetics which depend on the page it self
+    // (either because of the hostname or content of the DOM). Content script
+    // logic is responsible for returning information about lists of classes,
+    // ids and hrefs observed in the DOM. MutationObserver is also used to
+    // make sure we can react to changes.
+    {
+      const { active, styles, scripts } = this.getCosmeticsFilters({
+        domain,
+        hostname,
+        url,
+
+        classes: msg.classes,
+        hrefs: msg.hrefs,
+        ids: msg.ids,
+
+        // This needs to be done only once per frame
+        getBaseRules: false,
+        getInjectionRules: msg.lifecycle === 'start',
+        getRulesFromHostname: msg.lifecycle === 'start',
+
+        // This will be done every time we get information about DOM mutation
+        getRulesFromDOM: msg.lifecycle === 'dom-update',
+      });
+
+      if (active === false) {
+        return;
+      }
+
+      this.injectStyles(e.sender, styles);
+
+      // Inject scripts from content script
+      const responseFromBackground: IMessageFromBackground = {
+        active,
+        extended: [],
+        scripts,
+        styles: '',
+      };
+
+      e.sender.send(`get-cosmetic-filters-${id}`, responseFromBackground);
+    }
+
   }
 
   private onRequest = (
@@ -34,4 +122,13 @@ export default class ElectronBlocker extends Engine {
       callback({ cancel: match });
     }
   };
+
+  private injectStyles(
+    sender: Electron.WebContents,
+    styles: string,
+  ): void {
+    if (sender && styles.length > 0) {
+      sender.insertCSS(styles);
+    }
+  }
 }
