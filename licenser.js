c2ec93b7d75eb86b4f97ab9aeaef035868a91f2e
Rémi Berson <remi@cliqz.com>
2019-07-03 15:00:47 +0200

    docs: add license notice in lerna-lint.js and licenser.js

--- licenser.js
+++ licenser.js
@@ -1,3 +1,11 @@
+/*!
+ * Copyright (c) 2017-2019 Cliqz GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const { promises } = require('fs');
 const glob = require('glob');
 
@@ -8,6 +16,8 @@ const BLACKLIST = [
 ];
 
 const PATTERNS = [
+  './licenser.js',
+  './lerna-lint.js',
   './packages/*/LICENSE',
   './packages/*/jest.config.js',
   './packages/*/{src,test,example}/**/*.ts',
