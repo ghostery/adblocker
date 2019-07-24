635037db2d8bf3aea9be3849a7ef994964a73342
Rémi <remusao@users.noreply.github.com>
2019-07-24 19:26:33 +0200

    Release v0.12.0 (#214)

--- packages/adblocker/src/engine/engine.ts
+++ packages/adblocker/src/engine/engine.ts
@@ -16,7 +16,7 @@ import Resources from '../resources';
 import CosmeticFilterBucket from './bucket/cosmetic';
 import NetworkFilterBucket from './bucket/network';
 
-export const ENGINE_VERSION = 29;
+export const ENGINE_VERSION = 30;
 
 // Polyfill for `btoa`
 function btoaPolyfill(buffer: string): string {
