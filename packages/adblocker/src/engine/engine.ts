598d63d269656c31a5bb68ad4aa78b80c1ef54c4
Rémi Berson <remi@cliqz.com>
2019-08-27 17:52:52 +0200

    bump FiltersEngine version

--- packages/adblocker/src/engine/engine.ts
+++ packages/adblocker/src/engine/engine.ts
@@ -25,7 +25,7 @@ import Resources from '../resources';
 import CosmeticFilterBucket from './bucket/cosmetic';
 import NetworkFilterBucket from './bucket/network';
 
-export const ENGINE_VERSION = 34;
+export const ENGINE_VERSION = 35;
 
 // Polyfill for `btoa`
 function btoaPolyfill(buffer: string): string {
