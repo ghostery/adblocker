fef4048d405eb0756ea9c4bda355f128e5504886
Rémi Berson <remi@cliqz.com>
2020-01-15 16:17:44 +0100

    Detect if CHANGELOG had no update

--- actions/generate-changelog/main.js
+++ actions/generate-changelog/main.js
@@ -1,20 +1,27 @@
-const { writeFileSync } = require('fs');
+const { writeFileSync, readFileSync } = require('fs');
 const { resolve } = require('path');
 
 const { execSync } = require('child_process');
 execSync(`cd ${__dirname}; npm ci`);
 
+const core = require('@actions/core');
+
 const currentTag = execSync(`git describe --abbrev=0 --tags ${process.env.GITHUB_SHA}`)
   .toString()
   .trim();
 
+const CHANGELOG_PATH = resolve(process.env.GITHUB_WORKSPACE, 'CHANGELOG.md');
+const CHANGELOG =   execSync(
+  `node ${resolve(
+    __dirname,
+    'node_modules/.bin/lerna-changelog',
+  )} --from v0.12.0 --to ${currentTag}`,
+).toString().trim();
+
+core.setOutput("updated", CHANGELOG !== readFileSync(CHANGELOG_PATH, 'utf-8'));
+
 writeFileSync(
-  resolve(process.env.GITHUB_WORKSPACE, 'CHANGELOG.md'),
-  execSync(
-    `node ${resolve(
-      __dirname,
-      'node_modules/.bin/lerna-changelog',
-    )} --from v0.12.0 --to ${currentTag}`,
-  ).toString().trim(),
+  CHANGELOG_PATH,
+  CHANGELOG,
   'utf-8',
 );
