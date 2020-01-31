3967d8fe70a60a3c61805d49224e8afe3487af83
Rémi <remusao@users.noreply.github.com>
2020-01-31 16:17:34 +0100

    Update assets (#439)

--- packages/adblocker/tools/generate_compression_codebooks.ts
+++ packages/adblocker/tools/generate_compression_codebooks.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 import { resolve } from 'path';
-import { generate } from 'tsmaz';
+import { generate, Smaz } from 'tsmaz';
 
 import { parseFilters, NetworkFilter, CosmeticFilter } from '../adblocker';
 
@@ -43,22 +43,27 @@ async function getStrings(kind: string): Promise<string[]> {
   switch (kind) {
     case 'network-csp':
       return (await getNetworkFilters())
+        .filter((filter) => filter.isUnicode() === false)
         .map(({ csp }) => csp || '')
         .filter((csp) => csp.length !== 0);
     case 'network-redirect':
       return (await getNetworkFilters())
+        .filter((filter) => filter.isUnicode() === false)
         .map(({ redirect }) => redirect || '')
         .filter((redirect) => redirect.length !== 0);
     case 'network-filter':
       return (await getNetworkFilters())
+        .filter((filter) => filter.isUnicode() === false)
         .map(({ filter }) => filter || '')
         .filter((filter) => filter.length !== 0);
     case 'network-hostname':
       return (await getNetworkFilters())
+        .filter((filter) => filter.isUnicode() === false)
         .map(({ hostname }) => hostname || '')
         .filter((hostname) => hostname.length !== 0);
     case 'cosmetic-selector':
       return (await getCosmeticFilters())
+        .filter((filter) => filter.isUnicode() === false)
         .map(({ selector }) => selector || '')
         .filter((selector) => selector.length !== 0);
     default:
@@ -66,18 +71,74 @@ async function getStrings(kind: string): Promise<string[]> {
   }
 }
 
+function validateCodebook(codebook: string[], strings: string[]): void {
+  console.log('Validating codebook', codebook);
+  console.log(`Checking ${strings.length} strings...`);
+
+  const smaz = new Smaz(codebook);
+  let maxSize = 0;
+  let minSize = Number.MAX_SAFE_INTEGER;
+  let totalSize = 0;
+  let totalCompressed = 0;
+
+  for (const str of strings) {
+    const compressed = smaz.compress(str);
+    const original = smaz.decompress(compressed);
+    if (original !== str) {
+      throw new Error(`Mismatch: ${str} vs. ${original} (compressed: ${compressed})`);
+    }
+
+    totalSize += str.length;
+    totalCompressed += compressed.length;
+    maxSize = Math.max(maxSize, str.length);
+    minSize = Math.min(minSize, str.length);
+  }
+
+  console.log('Codebook validated:', {
+    maxSize,
+    minSize,
+    totalSize,
+    totalCompressed,
+    compressionRatio: 100.0 * ((totalSize - totalCompressed) / totalSize),
+  });
+}
+
 async function generateCodebook(kind: string): Promise<string[]> {
   const strings = await getStrings(kind);
   console.log(`Generate codebook ${kind} using ${strings.length} strings.`);
-  return generate(strings);
+  const codebook = generate(strings);
+  validateCodebook(codebook, strings);
+  return codebook;
 }
 
 (async () => {
   const kind = process.argv[process.argv.length - 1];
   const codebook = await generateCodebook(kind);
+  const output = resolve(__dirname, `../src/codebooks/${kind}.ts`);
+  console.log('Updating', output);
   await fs.writeFile(
-    resolve(__dirname, `../src/codebooks/${kind}.ts`),
-    `/* tslint:disable */\nexport default ${JSON.stringify(codebook, null, 2)};\n`,
+    output,
+    [
+      '/*!',
+      ' * Copyright (c) 2017-present Cliqz GmbH. All rights reserved.',
+      ' *',
+      ' * This Source Code Form is subject to the terms of the Mozilla Public',
+      ' * License, v. 2.0. If a copy of the MPL was not distributed with this',
+      ' * file, You can obtain one at https://mozilla.org/MPL/2.0/.',
+      ' */',
+      '/* tslint:disable quotemark */',
+      `export default ${JSON.stringify(
+        codebook.sort((str1, str2) => {
+          if (str1.length !== str2.length) {
+            return str2.length - str1.length;
+          }
+
+          return str1.localeCompare(str2);
+        }),
+        null,
+        2,
+      )};`,
+    ].join('\n'),
     'utf-8',
   );
 })();
