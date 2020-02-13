9267ef52b81b58e52990baade087b4ab63537fe6
Rémi Berson <remi@cliqz.com>
2020-02-13 10:23:03 +0100

    Bump internal engine representation

--- packages/adblocker/src/engine/engine.ts
+++ packages/adblocker/src/engine/engine.ts
@@ -27,7 +27,7 @@ import Resources from '../resources';
 import CosmeticFilterBucket from './bucket/cosmetic';
 import NetworkFilterBucket from './bucket/network';
 
-export const ENGINE_VERSION = 46;
+export const ENGINE_VERSION = 47;
 
 // Polyfill for `btoa`
 function btoaPolyfill(buffer: string): string {
