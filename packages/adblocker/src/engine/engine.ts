cb99b65d1a41a4f6f3f0056b5090ad836f51114e
Rémi Berson <remi@cliqz.com>
2021-01-21 12:35:00 +0100

    Update compression codebooks

--- packages/adblocker/src/engine/engine.ts
+++ packages/adblocker/src/engine/engine.ts
@@ -29,7 +29,7 @@ import Resources from '../resources';
 import CosmeticFilterBucket from './bucket/cosmetic';
 import NetworkFilterBucket from './bucket/network';
 
-export const ENGINE_VERSION = 364;
+export const ENGINE_VERSION = 365;
 
 function shouldApplyHideException(filters: NetworkFilter[]): boolean {
   if (filters.length === 0) {
