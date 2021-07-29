2b1f660b3068e47a3ca74a17f96c123e5a86b9ac
Manish Jethani <code@manishjethani.io>
2021-07-29 13:45:25 +0530

    Use Node.js version of uBlock Origin (#2075)
    * Use Node.js version of uBlock Origin
    
    * Install the package into node_modules
    
    * Update .gitignore
    
    * Add .npmrc
    
    * Update .gitignore
    
    * Remove unused imports
    
    * Ignore specific blocker subdirectories
    
    * Remove unused dependencies
    
    * Use --depth 1 for uAssets

--- packages/adblocker-benchmarks/blockers/ublock.js
+++ packages/adblocker-benchmarks/blockers/ublock.js
@@ -6,165 +6,62 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const { URL } = require('url');
-const punycode = require('punycode');
-const fs = require('fs');
-const path = require('path');
+const modulePromise = import('ubo-snfe');
 
-const SandboxedModule = require('sandboxed-module');
-const { JSDOM } = require('jsdom');
-
-const DOM = new JSDOM('<!DOCTYPE html>background');
-
-// MOCK: assets
-const assetsStore = new Map();
-const assets = {
-  get: (key, cb) => {
-    const result = { content: assetsStore.get(key) };
-    if (cb) {
-      cb(result);
-    }
-    return Promise.resolve(result);
-  },
-  put: (key, value, cb) => {
-    assetsStore.set(key, value);
-    if (cb) {
-      cb();
-    }
-    return Promise.resolve();
-  },
-};
-
-// MOCK: localStorage
-const localStorageStore = new Map();
-const localStorage = {
-  getItem: key => localStorageStore.get(key),
-  setItem: (key, value) => { localStorageStore.set(key, value); },
-};
-
-// MOCK: requestIdleCallback
-DOM.window.requestIdleCallback = cb => setTimeout(cb, 1000);
-
-// MOCK: tabContextManager
-const tabContextManager = {
-  mustLookup: () => ({
-    origin: '',
-    rootHostname: '',
-    rootDomain: '',
-    tabId: -1,
-  }),
-};
-
-// MOCK: µBlock
-const µBlock = {
-  pageStores: new Map(),
-  getNetFilteringSwitch: () => true,
-  hiddenSettings: {
-    disableWebAssembly: false,
-  },
-  tabContextManager,
-  assets,
-  logger: {
-    enabled: false,
-    writeOne: () => {},
-  },
-  sessionSwitches: {
-    evaluateZ: () => true,
-  },
-};
-
-// MOCK: vAPI
-const vAPI = {
-  webextFlavor: {
-    soup: new Set(['firefox']),
-  },
-  localStorage,
-  setTimeout: () => {},
-  tabs: {
-    registerListeners: () => {},
-    onPopupCreated: () => {},
-  },
-  i18n: () => '',
-};
-
-
-const globals = {
-  URL,
-  self: {
-    punycode,
-  },
-  punycode,
-  µBlock,
-  vAPI,
-  self: DOM.window,
-  window: DOM.window,
-  document: DOM.window.document,
-};
-
-// NOTE: some changes were required to load `publicsuffixlist.js in this context.
-// Check `publicsuffixlist.patch` file for more information about what changed.
-globals.publicSuffixList = SandboxedModule.require('./ublock/publicsuffixlist.js', { globals });
-globals.publicSuffixList.parse(fs.readFileSync(path.resolve(__dirname, './ublock/public_suffix_list.dat'), { encoding: 'utf-8' }), punycode.toASCII);
-const publicSuffixListEnabledPromise = globals.publicSuffixList.enableWASM();
-
-// NOTE: some changes were required to load `hntrie.js` in this context. The
-SandboxedModule.require('./ublock/utils.js', { globals });
-SandboxedModule.require('./ublock/uritools.js', { globals });
-SandboxedModule.require('./ublock/strie.js', { globals });
-globals.STrieContainer = µBlock.STrieContainer;
-
-// HNTrieContainer needs to be attached to the global µBlock object to be
-// accessible by other modules.
-SandboxedModule.require('./ublock/hntrie.js', { globals });
-globals.HNTrieContainer = µBlock.HNTrieContainer;
-const { HNTrieContainerReadyPromise } = µBlock;
-
-SandboxedModule.require('./ublock/static-ext-filtering.js', { globals });
-SandboxedModule.require('./ublock/static-net-filtering.js', { globals });
-SandboxedModule.require('./ublock/storage.js', { globals });
-
-// NOTE: only `normalizePageURL` was kept in `tab.js`.
-SandboxedModule.require('./ublock/pagestore.js', { globals });
-SandboxedModule.require('./ublock/tab.js', { globals });
-SandboxedModule.require('./ublock/filtering-context.js', { globals });
-
-module.exports = class UBlockOrigin {
-  static async parse(rawLists) {
-    await publicSuffixListEnabledPromise;
-    await HNTrieContainerReadyPromise;
-
-    µBlock.staticNetFilteringEngine.fromCompiledContent(
-      new µBlock.CompiledLineIO.Reader(µBlock.compileFilters(rawLists)),
-    );
-    µBlock.staticNetFilteringEngine.freeze();
-    return new UBlockOrigin(µBlock.staticNetFilteringEngine);
+class MockStorage {
+  constructor(serialized) {
+    this.map = new Map(serialized);
   }
 
-  constructor(engine) {
+  async put(assetKey, content) {
+    this.map.set(assetKey, content);
+    return ({ assetKey, content });
+  }
+
+  async get(assetKey) {
+    return ({ assetKey, content: this.map.get(assetKey) });
+  }
+
+  *[Symbol.iterator]() {
+    yield* this.map;
+  }
+}
+
+module.exports = class UBlockOrigin {
+  static async initialize() {
+    let { pslInit } = await modulePromise;
+
+    pslInit();
+  }
+
+  static async parse(rawLists) {
+    let { FilteringContext, restart } = await modulePromise;
+
+    return new UBlockOrigin(restart([ { name: 'easylist', raw: rawLists } ]),
+                            new FilteringContext());
+  }
+
+  constructor(engine, context) {
     this.engine = engine;
+    this.context = context;
   }
 
   async serialize() {
-    await this.engine.toSelfie('path');
-    return JSON.stringify([
-      (await assets.get('path/main')).content,
-      (await assets.get('path/trieContainer')).content,
-    ]);
+    let storage = new MockStorage();
+    await this.engine.toSelfie(storage, 'path');
+    return JSON.stringify([...storage]);
   }
 
-  async deserialize() {
-    await this.engine.fromSelfie('path');
+  async deserialize(serialized) {
+    let storage = new MockStorage(JSON.parse(serialized));
+    await this.engine.fromSelfie(storage, 'path');
   }
 
   match({ url, frameUrl, type }) {
-    return (
-      this.engine.matchString(
-        µBlock.filteringContext.fromWebrequestDetails({
-          url,
-          type,
-          documentUrl: frameUrl,
-        }),
-      ) === 1
-    );
+    this.context.setDocOriginFromURL(frameUrl);
+    this.context.setURL(url);
+    this.context.setType(type);
+
+    return this.engine.matchRequest(this.context) === 1;
   }
 };
