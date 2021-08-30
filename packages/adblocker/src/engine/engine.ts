1aa394dee740f3f04015572b4582b301f40b2bfb
remusao <remusao@users.noreply.github.com>
2021-08-30 20:43:30 +0000

    Update local assets and compression codebooks

--- packages/adblocker/src/engine/engine.ts
+++ packages/adblocker/src/engine/engine.ts
@@ -29,7 +29,7 @@ import Resources from '../resources';
 import CosmeticFilterBucket from './bucket/cosmetic';
 import NetworkFilterBucket from './bucket/network';
 
-export const ENGINE_VERSION = 471;
+export const ENGINE_VERSION = 472;
 
 function shouldApplyHideException(filters: NetworkFilter[]): boolean {
   if (filters.length === 0) {
