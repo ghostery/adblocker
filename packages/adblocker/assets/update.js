e3ae6591a4cc1f01a33ad2ce83cb9657a5de4b8e
Rémi Berson <remi@brave.com>
2023-07-23 13:46:39 +0200

    Fix

--- packages/adblocker/assets/update.js
+++ packages/adblocker/assets/update.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const got = require('got');
 const adb = require('@cliqz/adblocker');
 
 (async () => {
+  const { got } = await import("got");
+  console.log(got);
+
   // Update resources.txt
   fs.writeFileSync(
     path.join(__dirname, 'ublock-origin', 'resources.txt'),
