d5e26a6598bceb6c9ad7db386b46f9ed2ce784df
Krzysztof Modras <chrmod@chrmod.net>
2024-02-13 12:16:33 +0100

    Update assets from Ghostery CDN (#3777)
    Provides scriptlets compatibility

--- packages/adblocker/assets/update.js
+++ packages/adblocker/assets/update.js
@@ -2,24 +2,46 @@ const fs = require('fs');
 const path = require('path');
 const adb = require('@cliqz/adblocker');
 
-(async () => {
-  const { revisions } = await fetch('https://cdn.ghostery.com/adblocker/resources/ublock-resources/metadata.json').then(result => {
+const FILTER_LISTS = [
+  ['ublock-unbreak', 'ublock-origin', 'unbreak.txt'],
+  ['ublock-quick-fixes', 'ublock-origin', 'quick-fixes.txt'],
+  ['ublock-filters', 'ublock-origin', 'filters.txt'],
+  ['ublock-filters-2020', 'ublock-origin', 'filters-2020.txt'],
+  ['ublock-filters-2021', 'ublock-origin', 'filters-2021.txt'],
+  ['ublock-filters-2022', 'ublock-origin', 'filters-2022.txt'],
+  ['ublock-filters-2023', 'ublock-origin', 'filters-2023.txt'],
+  ['ublock-filters-2024', 'ublock-origin', 'filters-2024.txt'],
+  ['ublock-badware', 'ublock-origin', 'badware.txt'],
+  ['ublock-abuse', 'ublock-origin', 'resource-abuse.txt'],
+  ['ublock-annoyances-others', 'ublock-origin', 'annoyances-others.txt'],
+  ['ublock-annoyances-cookies', 'ublock-origin', 'annoyances-cookies.txt'],
+  ['ublock-privacy', 'ublock-origin', 'privacy.txt'],
+  ['plowe-0', 'peter-lowe', 'serverlist.txt'],
+  ['easylist', 'easylist', 'easylist.txt'],
+  ['easyprivacy', 'easylist', 'easyprivacy.txt'],
+  ['easylist-cookie', 'easylist', 'easylist-cookie.txt'],
+];
+
+async function downloadResource(resourceName) {
+  const { revisions } = await fetch(`https://cdn.ghostery.com/adblocker/resources/${resourceName}/metadata.json`).then(result => {
     if (!result.ok) {
-      throw new Error(`Failed to uBO resources metadata: ${result.status}: ${result.statusText}`);
+      throw new Error(`Failed to fetch ${resourceName} metadata: ${result.status}: ${result.statusText}`);
     }
     return result.json();
   });
-  const uBOResources = await fetch(`https://cdn.ghostery.com/adblocker/resources/ublock-resources/${revisions.pop()}/list.txt`).then(result => {
+  return fetch(`https://cdn.ghostery.com/adblocker/resources/${resourceName}/${revisions.pop()}/list.txt`).then(result => {
     if (!result.ok) {
-      throw new Error(`Failed to uBO resources: ${result.status}: ${result.statusText}`);
+      throw new Error(`Failed to fetch ${resourceName}: ${result.status}: ${result.statusText}`);
     }
     return result.text();
   })
+}
 
+(async () => {
   // Update resources.txt
   fs.writeFileSync(
     path.join(__dirname, 'ublock-origin', 'resources.txt'),
-    uBOResources,
+    await downloadResource('ublock-resources'),
     'utf-8',
   );
 
@@ -30,122 +52,43 @@ const adb = require('@cliqz/adblocker');
   const seen = new Map();
 
   // Update lists
-  for (const [url, key] of [
-    // uBO
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt',
-      ['ublock-origin', 'unbreak.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/quick-fixes.txt',
-      ['ublock-origin', 'quick-fixes.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-mobile.txt',
-      ['ublock-origin', 'filters-mobile.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt',
-      ['ublock-origin', 'filters.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2020.txt',
-      ['ublock-origin', 'filters-2020.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2021.txt',
-      ['ublock-origin', 'filters-2021.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2022.txt',
-      ['ublock-origin', 'filters-2022.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2023.txt',
-      ['ublock-origin', 'filters-2023.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2024.txt',
-      ['ublock-origin', 'filters-2024.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt',
-      ['ublock-origin', 'badware.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt',
-      ['ublock-origin', 'resource-abuse.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances-others.txt',
-      ['ublock-origin', 'annoyances-others.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances-cookies.txt',
-      ['ublock-origin', 'annoyances-cookies.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt',
-      ['ublock-origin', 'annoyances.txt'],
-    ],
-    [
-      'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt',
-      ['ublock-origin', 'privacy.txt'],
-    ],
+  for (const [resourceName, outputPrefixPath, outputFileName] of FILTER_LISTS) {
+    console.log(`Fetching: ${resourceName}`);
 
-    // Peter Lowe
-    [
-      'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=0&mimetype=plaintext',
-      ['peter-lowe', 'serverlist.txt'],
-    ],
-
-    // Easylist
-    ['https://easylist.to/easylist/easyprivacy.txt', ['easylist', 'easyprivacy.txt']],
-    ['https://easylist.to/easylist/easylist.txt', ['easylist', 'easylist.txt']],
-    ['https://secure.fanboy.co.nz/fanboy-cookiemonster.txt', ['easylist', 'easylist-cookie.txt']],
-    [
-      'https://easylist.to/easylistgermany/easylistgermany.txt',
-      ['easylist', 'easylistgermany.txt'],
-    ],
-
-    // Fanboy
-    // [
-    //   'https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt',
-    //   ['fanboy', 'annoyance.txt'],
-    // ],
-  ]) {
-    const lines = (await fetch(url).then(r => r.text()))
+    const lines = (await downloadResource(resourceName))
       .split(/[\r\n]/g)
       .map((line) => line.trim())
       .map((line) => {
         const filter = adb.parseFilter(line);
+        const outputPath = `${outputPrefixPath}/${outputFileName}`;
 
         if (filter === null) {
           return line;
         }
 
+        // Count bad filters
         if (filter.isBadFilter?.()) {
-          badfilters.set(filter.getIdWithoutBadFilter(), `${key.join('/')}`);
+          badfilters.set(filter.getIdWithoutBadFilter(), outputPath);
           return `! [badfilter] ${line}`;
         }
-
         const badfilter = badfilters.get(filter.getIdWithoutBadFilter?.());
         if (badfilter !== undefined) {
           badfiltersCount += 1;
           return `! [badfilter] from ${badfilter}\n! ${line}`;
         }
 
+        // Count duplicates
         const dup = seen.get(filter.getId());
         if (dup !== undefined) {
           duplicatesCount += 1;
           return `! [dup] from ${dup}\n! ${line}`;
         }
+        seen.set(filter.getId(), outputPath);
 
-        seen.set(filter.getId(), `${key.join('/')}`);
         return line;
       });
 
-    fs.writeFileSync(path.join(__dirname, ...key), lines.join('\n'), 'utf-8');
+    fs.writeFileSync(path.join(__dirname, outputPrefixPath, outputFileName), lines.join('\n'), 'utf-8');
   }
 
   console.log({ duplicates: duplicatesCount, badfilters: badfiltersCount });
