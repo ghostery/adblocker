88f35a1c62a72987c42e312dbf6d568e4f22139c
HoJeong Go <seia@outlook.kr>
2025-01-23 16:06:14 +0900

    fix: convert to unsigned int on set

--- packages/adblocker/src/utils.ts
+++ packages/adblocker/src/utils.ts
@@ -27,7 +27,7 @@ export function getBit(n: number, mask: number): boolean {
 }
 
 export function setBit(n: number, mask: number): number {
-  return n | mask;
+  return (n | mask) >>> 0;
 }
 
 export function clearBit(n: number, mask: number): number {
