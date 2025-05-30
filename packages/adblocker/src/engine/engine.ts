e878d4c1a05c8af0acb624878c7439bd84978da8
HoJeong Go <seia@outlook.kr>
2025-05-30 16:50:33 +0900

    fix: replace `URLSearchParams` from `@ghostery/url-parser`
    refs https://github.com/ghostery/adblocker/pull/4528#discussion_r2100515247
    refs https://github.com/ghostery/url-parser/pull/679

--- packages/adblocker/src/engine/engine.ts
+++ packages/adblocker/src/engine/engine.ts
@@ -7,6 +7,7 @@
  */
 
 import type { IMessageFromBackground } from '@ghostery/adblocker-content';
+import { URLSearchParams } from '@ghostery/url-parser';
 
 import Config from '../config.js';
 import { StaticDataView, sizeOfASCII, sizeOfByte, sizeOfBool } from '../data-view.js';
