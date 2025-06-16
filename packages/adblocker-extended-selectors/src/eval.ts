55a96b78651d519d3c0a705bd6486699fcde28c7
Krzysztof Modras <1228153+chrmod@users.noreply.github.com>
2025-06-16 21:02:41 +0200

    Update packages/adblocker-extended-selectors/src/eval.ts
    Co-authored-by: Philipp Claßen <philipp.classen@posteo.de>

--- packages/adblocker-extended-selectors/src/eval.ts
+++ packages/adblocker-extended-selectors/src/eval.ts
@@ -26,7 +26,6 @@ function parseRegex(str: string): RegExp {
 }
 
 function stripsWrappingQuotes(str: string): string {
-  // wrapping quotes are optional
   if ((str.startsWith('"') && str.endsWith('"')) || (str.startsWith("'") && str.endsWith("'"))) {
     return str.slice(1, -1);
   }
