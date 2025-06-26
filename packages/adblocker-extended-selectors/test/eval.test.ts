6c845180289e85c38472465430b04f4e7f1b30b1
HoJeong Go <seia@outlook.kr>
2025-06-26 16:42:33 +0900

    fix: clarify `querySelectorAll` spec
    This code is intended to be matched with `document.documentElement.querySelectorAll`.

--- packages/adblocker-extended-selectors/test/eval.test.ts
+++ packages/adblocker-extended-selectors/test/eval.test.ts
@@ -472,7 +472,7 @@ describe('eval', () => {
     describe(':has', () => {
       it('*:has', () => {
         testQuerySelectorAll(
-          `*:has(a[href^="https://"]):not(html):not(body):not(p)`,
+          `*:has(a[href^="https://"]):not(body):not(p)`,
           [
             '<!DOCTYPE html>',
             '<p class="cls1">',
@@ -499,7 +499,7 @@ describe('eval', () => {
         );
 
         testQuerySelectorAll(
-          `:has(.banner):not(html):not(body)`,
+          `:has(.banner):not(body)`,
           [
             '<div>Do not select this div</div>',
             '<div id="res">Select this div<span class="banner"></span></div>',
@@ -550,7 +550,7 @@ describe('eval', () => {
     describe(':not', () => {
       it('not paragraph', () => {
         testQuerySelectorAll(
-          ':not(p):not(body):not(html):not(head)',
+          ':not(p):not(body):not(head)',
           [
             '<!DOCTYPE html>',
             '<head></head>',
