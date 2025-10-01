2f0303005798dd5332d39f4bb70d0da64ebb1de4
HoJeong Go <seia@outlook.kr>
2025-09-30 18:53:41 -0700

    chore: optional keep request interception boolean value

--- packages/adblocker-puppeteer/src/index.ts
+++ packages/adblocker-puppeteer/src/index.ts
@@ -89,9 +89,15 @@ export class BlockingContext {
     }
   }
 
-  public async disable(): Promise<void> {
+  public async disable({
+    keepRequestInterception = false,
+  }: { keepRequestInterception?: boolean | undefined } = {}): Promise<void> {
     if (this.blocker.config.loadNetworkFilters) {
       this.page.off('request', this.onRequest);
+
+      if (keepRequestInterception === false) {
+        await this.page.setRequestInterception(false);
+      }
     }
 
     if (this.blocker.config.loadCosmeticFilters) {
